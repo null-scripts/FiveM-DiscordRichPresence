3d158bbe594b9cde86f42ad37b50582edf7e6508
Null <roitogaming@gmail.com>
2025-06-22 22:32:56 +0900

    Update config.lua

--- FiveM-DiscordRichPresence/shared/config.lua
+++ FiveM-DiscordRichPresence/shared/config.lua
@@ -2,4 +2,21 @@ Config = {}
 
 Config.Discord = {
     ApplicationID = 'XXXXXXXXXXXXXXXXXX'  --PLZ Discord ApplicationID
+    small = 'small',           
+    big = 'big',                            
+    HoverTextBig = 'Null-Scripts',          
+    HoverTextSmall = 'dev-srv'            
+}
+
+Config.Buttom = {
+    label_0 = 'My Community',
+    url_0 = '',
+
+    label_1 = 'Null-City',
+    url_1 = ''
+}
+
+Config.Display = {
+    id = 'ID:',
+    PlayerName = 'キャラクター名'
 }
